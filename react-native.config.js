c78f11426e8ea3029c2b0d11530ce0fe09e6826f
Pholish <Rogue.Pholish@gmail.com>
2019-09-21 13:27:21 +0300

    Fix: removed android warnings

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   assets: ['./src/assets/fonts/'],
+  dependencies: {
+    'react-native-i18n': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink
+      },
+    },
+    'react-native-linear-gradient': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink
+      },
+    },
+    'react-native-splash-screen': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink
+      },
+    },
+    'react-native-svg': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink
+      },
+    },
+  },
 };
